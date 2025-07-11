41e1c51d34b3b7b3ca8b48e44abe821f8bfde0d8
Andreas Schueller <apschueller@gmail.com>
2025-07-11 14:00:07 -0400

    Better documentation and added shebang

--- fix_atom_names_mol2.py
+++ fix_atom_names_mol2.py
@@ -1,9 +1,17 @@
-# Antechamber does not work with atom names > 3 characters of lenght.
-# This Python3 script fixes this.
+#!/usr/bin/env python3
+#
+# Antechamber does not work with atom names > 3 characters of lenght and requieres unique atom names.
+#
+# This Python3 script renames and renumbers atoms.
 # Also sets any user charges to zero, as requiered by Antechamber.
-
+#
 # for f in structs/*_lig.mol2; do cp $f $f.bak; python3 fix_atom_names_mol2.py $f.bak > $f; done
-
+#
+# HISTORY
+# 2025-06-17    0.3    Andreas    Better documentation and added shebang
+# 2025-05       0.2    Andreas    Setting partial charges to zero
+# 2020-ish      0.1    Andreas    First version written for Francisca Duran
+#
 import sys
 
 with open(sys.argv[1]) as f:
