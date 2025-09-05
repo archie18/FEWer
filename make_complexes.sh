539fe0ddf5f824ace3ce0d50f35f71560f62e5ff
Andreas Schueller <apschueller@gmail.com>
2025-09-05 12:55:58 -0400

    Added commented code for generating sequential number. Also printing old and new ligand name.

--- make_complexes.sh
+++ make_complexes.sh
@@ -5,13 +5,16 @@ LIGS=130_top_seeds_*.sdf
 cd "$DIR"
 BASE=$(basename "$REC" .pdb)
 
+#NUM=1
 for LIG in $LIGS; do
     echo $LIG
     # Get sequential number
     NUM=$(echo "$LIG" | sed -E 's/.*[^0-9]([0-9]+)\.sdf/\1/')
     cp "$REC" "${BASE}_${NUM}_recep.pdb"
     cp "$LIG" "${BASE}_${NUM}_lig.sdf"
+    echo " ${BASE}_${NUM}_lig.sdf"
     cp "${BASE}_sslink.txt" "${BASE}_${NUM}_sslink.txt"
     cp "${BASE}_bonds.txt" "${BASE}_${NUM}_bonds.txt"
+    #((NUM++))
 done
 cd ..
